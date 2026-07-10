869022ce5c25709156a35cdd0dadb9218785a720
또니 <soyeon@bagsoyeon-ui-MacBookPro.local>
2026-07-10 09:48:10 +0900

    Auto-update gwangju live video ID to 6z8okkZQr70

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ window.LIVE_CONFIG = {
     },
     gwangju: {
       label: "광주",
-      youtubeVideoId: "FNir0mWk_Ns", // CCB광주 실시간 스트림
+      youtubeVideoId: "6z8okkZQr70", // CCB광주 실시간 스트림
     },
     all: {
       label: "전체",
