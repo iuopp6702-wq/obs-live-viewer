fa7c63d49c086553a78f2bf98beb37a7494a6d67
또니 <soyeon@bagsoyeon-ui-MacBookPro.local>
2026-07-13 08:27:32 +0900

    Auto-update all live video ID to 4GKa69DTXa4

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ window.LIVE_CONFIG = {
     },
     all: {
       label: "전체",
-      youtubeVideoId: "4jWArhs2U74", // ALL (생산기술팀) - watcher가 자동 갱신 예정 (관리자 권한 부여 후)
+      youtubeVideoId: "4GKa69DTXa4", // ALL (생산기술팀) - watcher가 자동 갱신 예정 (관리자 권한 부여 후)
     },
   },
 };
