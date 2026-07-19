8c80df95719d11de4d7760ed9c24ebc5f17e7b98
또니 <soyeon@bagsoyeon-ui-MacBookPro.local>
2026-07-20 08:18:03 +0900

    Auto-update all live video ID to Kw61Nk2dFYs

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ window.LIVE_CONFIG = {
     },
     all: {
       label: "전체",
-      youtubeVideoId: "6x9X_BcoIx4", // ALL (생산기술팀) - watcher가 자동 갱신 예정 (관리자 권한 부여 후)
+      youtubeVideoId: "Kw61Nk2dFYs", // ALL (생산기술팀) - watcher가 자동 갱신 예정 (관리자 권한 부여 후)
     },
   },
 };
